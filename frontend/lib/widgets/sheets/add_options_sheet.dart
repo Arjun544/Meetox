d28e7769a4c53f86da4cecd2d4c846e0b772e2f5
arjun544 <arjunsingh.mahar55@gmail.com>
2023-05-19 09:46:16 +0500

    Added options sheet

--- frontend/lib/widgets/sheets/add_options_sheet.dart
+++ frontend/lib/widgets/sheets/add_options_sheet.dart
@@ -7,19 +7,76 @@ class AddOptionsSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: Get.height * 0.3,
+      height: Get.height * 0.28,
       padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
       decoration: BoxDecoration(
-        color: context.theme.dialogBackgroundColor,
+        color: context.theme.bottomSheetTheme.backgroundColor,
         borderRadius: BorderRadius.circular(20),
       ),
       child: Column(
         children: [
-          Text(
-            'Click to select an avatar',
-            style: context.theme.textTheme.labelSmall,
+          ListTile(
+            minLeadingWidth: 0,
+            contentPadding: EdgeInsets.zero,
+            dense: true,
+            leading: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: AppColors.primaryYellow.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Icon(
+                IconsaxBold.document_text,
+                color: AppColors.primaryYellow,
+              ),
+            ),
+            title: Text(
+              'Add post',
+              style: context.theme.textTheme.labelMedium,
+            ),
           ),
           SizedBox(height: 20.sp),
+          ListTile(
+            minLeadingWidth: 0,
+            contentPadding: EdgeInsets.zero,
+            dense: true,
+            leading: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.lightBlue.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Icon(
+                FlutterRemix.bubble_chart_fill,
+                color: Colors.lightBlue,
+              ),
+            ),
+            title: Text(
+              'Create circle',
+              style: context.theme.textTheme.labelMedium,
+            ),
+          ),
+          SizedBox(height: 20.sp),
+          ListTile(
+            minLeadingWidth: 0,
+            contentPadding: EdgeInsets.zero,
+            dense: true,
+            leading: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.green.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Icon(
+                FlutterRemix.question_mark,
+                color: Colors.green,
+              ),
+            ),
+            title: Text(
+              'Ask question',
+              style: context.theme.textTheme.labelMedium,
+            ),
+          ),
         ],
       ),
     );
